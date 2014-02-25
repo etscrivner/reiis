e9fb4289872c937a4e4f2e150ab2138045b5fb79
Eric Scrivner <zenogais@gmail.com>
2014-02-24 22:17:04 -0800

    Add initial exceptions file with base class Error

---
+++ reiis/exceptions.py
@@ -0,0 +1,7 @@
+"""In order to properly encapsulate errors specfic to this library itself we
+provide a unified set of exceptions descended from a common base class.
+
+"""
+
+class Error(Exception):
+    pass
